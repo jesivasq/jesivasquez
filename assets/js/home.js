d9701c9604ef8ec1bd95c3313478b53c32532243
Jesi <jesivasq@users.noreply.github.com>
2022-03-24 01:37:25 -0400

    animate: jesi and vasquez on home page
    a little animation where the Jesi drops down, bumps the Vasquez down, and bounces out

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,22 @@
+// turn the gear
 let gear = document.getElementById("gear");
+gsap.to(gear, {
+  rotate: 360,
+  duration: 4,
+  repeat: -1,
+  yoyo: true,
+  ease: "power2.inOut",
+});
 
-gsap.to(gear, { rotate: 360, duration: 4, repeat: -1, yoyo: true, ease: "power2.inOut" });
+let jesi = document.getElementById("jesi");
+gsap.from(jesi, { y: -500, duration: 2, ease: "bounce.out" });
+
+let vasquez = document.getElementById("vasquez");
+gsap.to(vasquez, {
+  y: 15,
+  duration: 0.15,
+  delay: 0.75,
+  repeat: 1,
+  yoyo: true,
+  ease: "power2.out",
+});
