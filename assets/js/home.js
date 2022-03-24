93e9f204adbce3e6583dd9cbcb3a7f995b1e7e08
Jesi <jesi.vasq@gmail.com>
2022-03-24 00:36:03 -0400

    updated ease on cog

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,3 @@
 let gear = document.getElementById("gear");
 
-gsap.to(gear, { rotate: 360, duration: 4 }).repeat(-1);
+gsap.to(gear, { rotate: 360, duration: 4, repeat: -1, yoyo: true, ease: "power2.inOut" });
