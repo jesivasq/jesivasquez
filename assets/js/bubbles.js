4fb8aee472f7e8685814efe7a9ec7b88bbbc8619
Jesi <jesivasq@users.noreply.github.com>
2022-03-27 16:33:04 -0400

    cleanup

--- assets/js/bubbles.js
+++ assets/js/bubbles.js
@@ -1,36 +1,36 @@
 class Bubble {
-    constructor(x, y){
-        this.offsetX = x;
-        this.offsetY = y;
-        this.theta = 0;
-        this.scalar = 10;
-        this.speed = Math.random()/10;
-        this.d = 20;
-        this.opacity = 255 * Math.random();
-    }
+  constructor(x, y) {
+    this.offsetX = x;
+    this.offsetY = y;
+    this.theta = 0;
+    this.scalar = 10;
+    this.speed = 0.05;
+    this.d = 20;
+    this.opacity = 245 * Math.random() + 10;
+  }
 
-    update(){
-        this.theta += this.speed;
-        this.x = this.scalar * sin(this.theta) + this.offsetX;
-        this.y = this.scalar * cos(this.theta) + this.offsetY;
-        fill(255, 255, 255, this.opacity);
-        ellipse(this.x, this.y, this.d, this.d);
-    }
+  update() {
+    this.theta += this.speed;
+    this.x = this.scalar * sin(this.theta) + this.offsetX;
+    this.y = this.scalar * cos(this.theta) + this.offsetY;
+    fill(255, 255, 255, this.opacity);
+    ellipse(this.x, this.y, this.d, this.d);
+  }
 }
 
 let bubbles = [];
 
-function setup(){
-    createCanvas(400, 400);
-    noStroke();
+function setup() {
+  createCanvas(400, 400);
+  noStroke();
 }
 
-function draw(){
-    background(200, 230, 255);
-    bubbles.forEach(bubble => bubble.update());
+function draw() {
+  background(200, 230, 255);
+  bubbles.forEach((bubble) => bubble.update());
 }
 
-function mousePressed(){
-    let bubble = new Bubble(mouseX, mouseY);
-    bubbles.push(bubble);
+function mousePressed() {
+  let bubble = new Bubble(mouseX, mouseY);
+  bubbles.push(bubble);
 }
